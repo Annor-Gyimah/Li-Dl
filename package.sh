743dcd24ab8153728a60fb55f0f6674b6f2f7b40
Annor-Gyimah <eannor707@gmail.com>
2024-10-25 11:32:10 +0000

    Add icons folder

---
+++ package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Create folders.
+[ -e package ] && rm -r package
+mkdir -p package/opt
+mkdir -p package/opt/main
+mkdir -p package/usr/share/applications
+mkdir -p package/usr/share/icons/hicolor/scalable/apps
+
+# Copy files (change icon names, add lines for non-scaled icons)
+cp -r ../Exec/main package/opt/main/
+cp ../Exec/penguin.png package/usr/share/icons/hicolor/scalable/apps/penguin.png
+cp ../Exec/main.desktop package/usr/share/applications
+
+# Change permissions
+find package/opt/main -type f -exec chmod 644 -- {} +
+find package/opt/main -type d -exec chmod 755 -- {} +
+find package/usr/share -type f -exec chmod 644 -- {} +
+chmod +x package/opt/main/main
